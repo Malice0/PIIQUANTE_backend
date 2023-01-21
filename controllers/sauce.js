d592622d854d065add5027c408278831202a4ac4
Malice0 <Amalicia29@gmail.com>
2023-01-20 22:44:47 -0300

    getOneSauce ok

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,8 +1,19 @@
 const Sauce = require("../models/Sauce");
 const fs = require("fs");
 
-exports.likeSauce = (req, res, next) => {
+exports.likeSauce = (req, res, next) => {};
 
+
+exports.getOneSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.status(404).json({ error }));
+};
+
+exports.getAllSauce = (req, res, next) => {
+  Sauce.find()
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.statut(400).json({ error }));
 };
 
 exports.createSauce = (req, res, next) => {
@@ -29,31 +40,29 @@ exports.createSauce = (req, res, next) => {
 };
 
 exports.modifySauce = (req, res, next) => {
-  const sauceObject = req.file ? {
+  const sauceObject = req.file
+    ? {
         ...JSON.parse(req.body.sauce),
         imageUrl: `${req.protocol}://${req.get("host")}/images/${
           req.file.filename
         }`,
-        likes: "0",
-        dislikes: "0",
-        usersLiked: "[]",
-        usersDisliked: "[]",
-      } : { ...req.body };
-      delete sauceObject._userId;
-      Sauce.findOne({ _id: req.params.id })
-        .then((sauce) => {
-          if (sauce.userId != req.auth.userId) {
-            res.status(401).json({ message: "Non-autorisé" });
-          } else {
-            Sauce.updateOne(
-              { _id: req.params.id },
-              { ...sauceObject, _id: req.params.id }
-            )
-              .then(() => res.status(200).json({ message: "Objet modifié" }))
-              .catch((error) => req.status(401).json({ error }));
-          }
-        })
-        .catch((error) => res.status(400).json({ error }));
+      }
+    : { ...req.body };
+  delete sauceObject._userId;
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => {
+      if (sauce.userId != req.auth.userId) {
+        res.status(401).json({ message: "Non-autorisé" });
+      } else {
+        Sauce.updateOne(
+          { _id: req.params.id },
+          { ...sauceObject, _id: req.params.id }
+        )
+          .then(() => res.status(200).json({ message: "Objet modifié" }))
+          .catch((error) => req.status(401).json({ error }));
+      }
+    })
+    .catch((error) => res.status(400).json({ error }));
 };
 
 exports.deleteSauce = (req, res, next) => {
@@ -71,16 +80,4 @@ exports.deleteSauce = (req, res, next) => {
       }
     })
     .catch((error) => res.status(500).json({ error }));
-};
-
-exports.getOneSauce = (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id })
-    .then((sauce) => res.status(200).json(sauce))
-    .catch((error) => res.status(404).json({ error }));
-};
-
-exports.getAllSauce = (req, res, next) => {
-  Sauce.find()
-    .then((sauce) => res.status(200).json(sauce))
-    .catch((error) => res.statut(400).json({ error }));
 };
--- route/sauce.js
+++ route/sauce.js
@@ -6,11 +6,11 @@ const multer = require("../middleware/multer-config");
 
 const sauceCtrl = require("../controllers/sauce");
 
-router.post("/:id/like", auth, sauceCtrl.likeSauce);
+router.post("/:id/like", auth, multer, sauceCtrl.likeSauce);
+router.get("/:id", auth, sauceCtrl.getOneSauce);
+router.get("/", auth, sauceCtrl.getAllSauce);
 router.post("/", auth, multer, sauceCtrl.createSauce);
 router.put("/:id", auth, multer, sauceCtrl.modifySauce);
 router.delete("/:id", auth, multer, sauceCtrl.deleteSauce);
-router.get("/:id", auth, sauceCtrl.getOneSauce);
-router.get("/", auth, sauceCtrl.getAllSauce);
 
 module.exports = router;
